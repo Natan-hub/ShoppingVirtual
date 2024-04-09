24ad2f5693d001352529e5b6cd5522b925f2547c
Natan Loss <natan@tomticket.com>
2024-04-09 17:33:36 -0300

    First commit

---
+++ android/app/src/main/kotlin/br/com/loss/flutter_shoppe/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.loss.flutter_shoppe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/flutter_shoppe/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.loss.lojavirtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
